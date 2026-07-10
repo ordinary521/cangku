33a7b7b8563acd580aeb2d4794985e24a2ddab7f
ordinary521 <1207481794@qq.com>
2026-07-10 23:46:59 +0800

    Update api.php

--- api.php
+++ api.php
@@ -1,8 +1,28 @@
 <?php
-$ch = curl_init("https://v1.apizero.cn/api/wallpaper?category=%E7%BE%8E%E5%A5%B3&resolution=1920x1080&count=1");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
-curl_setopt($ch, CURLOPT_HTTPHEADER, ['Authorization: Bearer sk_test_6d00a7db9c5649c5847bdba769cf752c3812fd59a1c1c281']);
-$response = curl_exec($ch);
-curl_close($ch);
-echo $response;
+//此php和保存链接的txt文件放在同一目录下
+$filename = "img.txt";  /*保存链接的txt文件名*/
+if(!file_exists($filename)){
+    die('文件不存在');
+}
+//从文本获取链接
+$pics = [];
+$fs = fopen($filename, "r");
+while(!feof($fs)){
+    $line=trim(fgets($fs));
+    if($line!=''){
+        array_push($pics, $line);
+    }
+}
+//从数组随机获取链接
+$pic = $pics[array_rand($pics)];
+//返回指定格式
+$type=$_GET['type'];
+switch($type){
+//JSON返回
+case 'json':
+    header('Content-type:text/json');
+    die(json_encode(['pic'=>$pic]));
+default:
+    die(header("Location: $pic"));
+}
+?>
