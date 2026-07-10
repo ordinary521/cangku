0e942c810081b6e19304213ef6d102ce4b1b1d2b
ordinary521 <1207481794@qq.com>
2026-07-10 23:02:15 +0800

    Refactor API call to use GET method with headers

--- api.php
+++ api.php
@@ -1,16 +1,8 @@
 <?php
-$url = "https://v1.apizero.cn/api/wallpaper?" . http_build_query([
-    "category" => "美女",
-    "resolution" => "1920x1080",
-    "count" => "1",
-    "key" => "sk_test_6d00a7db9c5649c5847bdba769cf752c3812fd59a1c1c281",
-]);
-
-$ch = curl_init($url);
+$ch = curl_init("https://v1.apizero.cn/api/wallpaper?category=%E7%BE%8E%E5%A5%B3&resolution=1920x1080&count=1");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_TIMEOUT, 15);
-$body = curl_exec($ch);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+curl_setopt($ch, CURLOPT_HTTPHEADER, ['Authorization: Bearer sk_test_6d00a7db9c5649c5847bdba769cf752c3812fd59a1c1c281']);
+$response = curl_exec($ch);
 curl_close($ch);
-
-$data = json_decode($body, true);
-print_r($data);
+echo $response;
